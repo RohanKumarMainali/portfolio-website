a17f1d4898dfc5793969bcf9d526b7b798a314db
RohanKumarMainali <92391187+RohanKumarMainali@users.noreply.github.com>
2022-05-25 20:32:21 +0545

    Added links

--- front/src/Components/Projects.js
+++ front/src/Components/Projects.js
@@ -30,7 +30,7 @@ return(
         <p>Travel Nepal website was built using django for backend and react.js for frontend. This website features two different panel, one for user and one for admin. The admin panel is
             responsible for handling all the CRUD functionality of tour packages and booking request whereas the user panel displays all the available tour packages and it's
              details like available seats, price, description of each days and discount offers. I used django rest framework for creating all APIs and postman to test them
-             before using those APIs. Indeed I faced a lot of challenges and bugs on this project but I completed this project in time. </p>
+             before using those APIs. Indeed I faced a lot of challenges and bugs on this project but I completed this project in time. You can checkout repository of this project <a className={ProjectsStyle.figma} href="https://github.com/RohanMainaliOfficial/TourManagementSystem/tree/Rohan">here.</a> </p>
         </div>
 
         </div>
@@ -43,7 +43,7 @@ return(
         <p>Paisa Khai? is a simple expense tracking application that helps people to track all of their expenses. I built this prototype of Paisa Khai? application
          in figma with a user-centric design process. Before even jumping into the high fidelity prototype, I created user persona to
          understand my target audience. In addition to user persona, I also created wireframes, user flow and task flow to built an efficient design.
-          In this prototype, I mainly focused on usability and its principle for a pleasant user experience. </p>
+          In this prototype, I mainly focused on usability and its principle for a pleasant user experience. You can checkout the prototype <a className={ProjectsStyle.figma} href="https://www.figma.com/proto/HhNUdU53O5FejMoGpucuSs/Design-and-prototype?node-id=2%3A320&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A320">here.</a></p>
         </div>
 
         <div className={ProjectsStyle.right} data-aos="fade-up">
